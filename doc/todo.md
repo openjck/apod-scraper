5a1afc39f94cae014ca70bc0ef1d8a37fab3bd34
John Karahalis <john.karahalis@gmail.com>
2013-05-12 16:59:28 -0400

    Fix encoding issues. Scrape all APOD entries.

--- doc/todo.md
+++ doc/todo.md
@@ -1,5 +1,3 @@
 * Scrape credit data (see [credit.md](credit.md))
-* Store only the date portion of the ISO-formatted datetime (YYYY-MM-DD), or
-  adjust it to midnight of a the timezone that might make sense for the APOD
-  website
-* Fix bug where the 1999-06-10 entry cannot be scraped due to encoding
+* Prepend all links that appear in the explanation with the APOD base address
+* Capture videos
--- scraper.py
+++ scraper.py
@@ -1,44 +1,47 @@
-import scraperwiki, re, dateutil.parser as parser
 from bs4 import BeautifulSoup
+import scraperwiki
+import re
+import dateutil.parser as parser
 
-# Reformat markup to get rid of arbitrary linebreaks.
+# Reformat markup to remove arbitrary linebreaks.
 def oneline(html):
     return re.sub('\s+', ' ', html)
 
 base = 'http://apod.nasa.gov/apod/'
-
 archive = base + 'archivepix.html'
-archive_soup = BeautifulSoup(scraperwiki.scrape(archive))
+encoding = 'latin-1'
 
-apod_links = archive_soup.find_all(href=re.compile('ap.*[0-9]+\.html'))
-for apod_link in apod_links:
-    apod_html = scraperwiki.scrape(base + apod_link['href'])
-    apod_soup = BeautifulSoup(apod_html)
-
-    regex_flags = re.DOTALL | re.IGNORECASE
+archive_soup = BeautifulSoup(scraperwiki.scrape(archive), from_encoding=encoding)
+archive_links = archive_soup.find_all(href=re.compile('ap.*[0-9]+\.html'))
+for archive_link in archive_links:
+    apod_html = scraperwiki.scrape(base + archive_link['href'])
+    apod_soup = BeautifulSoup(apod_html, from_encoding=encoding)
 
     # URL
-    url = base + apod_link['href']
+    url = base + archive_link['href']
 
     # Date
-    date_raw = apod_link.previous_sibling[:-3]
-    date = parser.parse(date_raw).isoformat()
+    date_raw = archive_link.previous_sibling[:-3]
+    date = parser.parse(date_raw).strftime('%Y-%m-%d')
 
     # Title
-    title_enc = apod_link.text
-    title = title_enc.decode('latin-1')
+    title = archive_link.text
 
     # Explanation
-    explanation_enc = re.search('<(b|(h3))>.*?Explanation.*?</(b|(h3))>\s*(.*?)\s*(</p>)?<p>', apod_html, regex_flags).group(5)
-    explanation_dec = explanation_enc.decode('latin-1')
-    explanation = '<p>' + oneline(explanation_dec) + '</p>'
+    explanation_raw = re.search('<(b|(h3))>.*?Explanation.*?</(b|(h3))>\s*(.*?)\s*(</p>)?<p>', apod_html, re.DOTALL | re.IGNORECASE).group(5)
+    explanation_utf = explanation_raw.decode(encoding) # Remember, we are working directly with apod_html, which was not passed through / decoded by BeautifulSoup.
+    explanation = '<p>' + oneline(explanation_utf) + '</p>'
 
-    # Picture. Note that this ignores videos (at least for now).
+    # Picture URL. Check that there actually is a picture, as NASA sometimes
+    # publishes videos instead.
     picture_link = apod_soup.find(href=re.compile('image/'))
     if picture_link:
         picture_url = base + picture_link['href']
+        picture_found = True
+    else:
+        picture_found = False
 
     # Save!
-    if 'picture_url' in locals():
+    if picture_found:
         record = {'url': url, 'date': date, 'title': title, 'explanation': explanation, 'picture_url': picture_url}
         scraperwiki.sqlite.save(['url'], record)
