27298894e913d261c5364eb914898ce244d5a4a0
daniel-beckham <beckhamdaniels@gmail.com>
2016-03-14 14:44:50 -0400

    Change version number to '1.1.0'

--- scraper.py
+++ scraper.py
@@ -168,7 +168,7 @@ def table_exists(table):
 def main():
     # Change this number when the scraping algorithm changes. All pages will be
     # re-scraped.
-    version = '1.0.0'
+    version = '1.1.0'
 
     path = 'http://apod.nasa.gov/apod/'
     site_encoding = 'windows-1252'
